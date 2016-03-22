d7cdccde457e8b30c38329de74498f190fbf25dd
Aditya Ekawade <aditya.ekawade@gmail.com>
2016-03-22 22:05:31 +0530

    Create CoVariantReturnType.java

---
+++ CoVariantReturnType.java
@@ -0,0 +1,29 @@
+package P1;
+
+public class A {
+    int x = 10; 
+    void display() {
+        System.out.println("A");
+    }
+    A m1() {
+        return new A();
+    }
+}
+
+class B extends A {
+    int x = 20; 
+    void display() {
+        System.out.println("B");
+    }
+    
+    A m1() {
+        return new B();
+    }
+    
+    public static void main(String[] args) {
+        A a = new B(); // creates polymorphic object
+        System.out.println(a.m1().x); //10 [a.m1() - poly, then follow the reference]
+        a.display(); //B [polymorphic method call]
+        a.m1().display(); //B  [a.m1() - poly, m1() method is overrided and subclass is executed
+    }
+}
