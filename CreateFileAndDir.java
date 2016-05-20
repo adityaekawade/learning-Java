9bba554ae3d515e1989384e764176652e74d7fe0
Aditya Ekawade <aditya.ekawade@gmail.com>
2016-05-20 23:18:22 +0530

    Update CreateFileAndDir.java

--- CreateFileAndDir.java
+++ CreateFileAndDir.java
@@ -8,12 +8,20 @@ import java.io.IOException;
 public class CreateFileAndDir {
     public static void main(String[] args) {
        try {
-  
+        
+        //Creates a directory
         File myDir = new File("c:/mydir");
         myDir.mkdir();
         
+        //Creates a files inside the directory
         File myFile = new File(myDir, "myFile.txt");
         myFile.createNewFile();
+        
+        // Insert text inside the file created 
+        try (PrintWriter pw = new PrintWriter(myFile)) {
+               pw.println("Hey World");
+               pw.flush();
+           }
         
        }
        catch (IOException e) {
